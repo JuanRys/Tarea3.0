126cf3992dd2cb3536e8886671063648abf9e46a
JuanRys <112053118+JuanRys@users.noreply.github.com>
2022-08-25 12:49:19 -0500

    Add files via upload

---
+++ Mayor de edad python.py
@@ -0,0 +1,14 @@
+# -*- coding: iso-8859-15 -*-
+# Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+# Es posible que el codigo generado no sea completamente correcto. Si encuentra
+# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+
+if __name__ == '__main__':
+	print "Mayor de 18 años"
+	edad = raw_input()
+	if edad>=18:
+		print "Eres mayor de edad"
+	else:
+		print "No eres mayor de edad"
+
